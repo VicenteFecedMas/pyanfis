056a9297d42a11d50bfb373bfa78f14e4860b7ea
VicenteFecedMas <vicente.feced.mas@gmail.com>
2026-01-21 11:13:16 +0100

    LSTSQ cleanup and docstrings added

--- pyanfis/algorithms/lstsq.py
+++ pyanfis/algorithms/lstsq.py
@@ -1,36 +1,56 @@
 """lstsq algorithm to compute a set of parameters given the input and output variables"""
-from typing import Optional
 import torch
 
 class LSTSQ(torch.nn.Module):
     """
-    Computes the vector x that approximately solves the equation a @ x = b
+    Computes the vector x that approximately solves the equation a @ x = b.
 
     Attributes
     ----------
-    n_vars : float
-        length of the "x" vector
-    shoulder : float
-        shoulder of the linear S function
-
-    Returns
-    -------
-    torch.tensor
-        a tensor of equal size to the input tensor
+    n_vars : int
+        number of variables.
+    alpha : float
+        alpha value for updating theta.
+    driver: str
+        driver for the lstsq algorithm.
     """
-    __slots__ = ["theta", "alpha", "driver"]
-    def __init__(self, n_vars: int, alpha: float = 0.001, driver: str = 'gels') -> None:
-        super().__init__() # type: ignore
-        self.theta: torch.Tensor = torch.zeros((n_vars, 1))
-        self.alpha: float = alpha
-        self.driver: str = driver
-    def forward(self, x: torch.Tensor, y: Optional[torch.Tensor] = None) -> None:
-        """This method will not return anything, instead it will compute
-        a new value of 'theta' on each pass that is called"""
+    __slots__ = [
+        "theta",
+        "alpha",
+        "driver"
+    ]
+    def __init__(
+            self,
+            n_vars: int,
+            alpha: float = 0.001,
+            driver: str = 'gels'
+        ) -> None:
+        super().__init__()
+        self.theta = torch.zeros((n_vars, 1))
+        self.alpha = alpha
+        self.driver = driver
+
+    def forward(
+            self,
+            x: torch.Tensor,
+            y: torch.Tensor|None = None
+        ) -> None:
+        """
+        Compute a new value of 'theta'.
+
+         Attributes
+        ----------
+        x : torch.Tensor
+            tensor with values multipliers to variables
+        y : torch.Tensor
+            tensor with values that are results
+        """
         for _ in range(x.size(0)):
-            new_theta: torch.Tensor = torch.linalg.lstsq(x, y, driver=self.driver).solution # type: ignore
-            if new_theta.dim() > 2: # type: ignore
-                new_theta = new_theta.mean(dim=0) # type: ignore
-            self.theta = (1 - self.alpha) * self.theta + self.alpha * new_theta # type: ignore
+            new_theta = torch.linalg.lstsq(x, y, driver=self.driver).solution
+
+            if new_theta.dim() > 2:
+                new_theta = new_theta.mean(dim=0)
+                
+            self.theta = (1 - self.alpha) * self.theta + self.alpha * new_theta
 
         return None
